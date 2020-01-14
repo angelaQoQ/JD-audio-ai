087acf394f3de4399a8d7b8031ba9c13afffff13
cuigang_qianduan <891257242@qq.com>
2020-01-14 16:10:09 +0800

    初始版本

---
+++ test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2020-01-13 10:01:37
+ * @LastEditTime: 2020-01-13 10:01:43
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \JD语音demo\JD语音测试代码\test.js
+ */
+console.log(123)
